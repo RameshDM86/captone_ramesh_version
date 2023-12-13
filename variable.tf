d83c60430d7a28e3de7d78f5d491ad408b30ca2c
Ramesh DM <ramesha0486@gmail.com>
2023-12-13 08:49:42 +0800

    fline

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 # S3 bucket name
 variable "bucket-name" {
-  default = "ramesh-sctp"
+  default = ""
 }
 
 # Domain name that you have registered
 variable "domain-name" {
-  default = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.us-east-1.amazonaws.com" // Modify as per your domain name
+  default = "" // Modify as per your domain name
 }
